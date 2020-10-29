c449e907c93821ac91d5eeec487450f932de890c
Jeff Lee <hellow.jeff@gmail.com>
2020-10-29 09:55:49 +0800

    Add user controller

---
+++ GO_Getting_Started/FirstProject/controllers/user.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+	"regexp"
+)
+
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
+
+// (uc userController) -> bind this function to the struct userController andd called uc.
+func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the User Controller!"))
+}
+
+// contructor. Golang doesn't have constructor, but we usually use a function to represent it. The function name start with new.
+func newUserController() *userController {
+	return &userController{
+		userIDPattern: regexp.MustCompile(`^/users/(\d+)/?`);
+	}
+}
