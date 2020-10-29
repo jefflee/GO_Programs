3697b2f56c8fa38feb078d81f9f8197d766e7640
Jeff Lee <hellow.jeff@gmail.com>
2020-10-29 09:17:38 +0800

    Add 2 functions

--- GO_Getting_Started/FirstProject/models/user.go
+++ GO_Getting_Started/FirstProject/models/user.go
@@ -7,6 +7,17 @@ type User struct {
 }
 
 var (
-	user   []*User
+	users  []*User
 	nextID = 1
 )
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(u User) (User, error) {
+	u.ID = nextID
+	nextID++
+	users = append(users, &u)
+	return u, nil
+}
