9784bcfe795d905ba0b14caf82bcd4786565ea6c
Jeff Lee <hellow.jeff@gmail.com>
2020-11-12 08:34:53 +0800

    add comment

--- The_Go_CLI_Playbook/Demo_BuildAndRun/03-race/main.go
+++ The_Go_CLI_Playbook/Demo_BuildAndRun/03-race/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		i := i
+		i := i // i is shared, so this line fix the problem. (Variable shadowing)
 		go func() {
 			fmt.Println(i)
 			wg.Done()
