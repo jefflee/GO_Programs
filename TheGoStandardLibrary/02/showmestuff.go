9091365d074a3012693a443ee74682a205398a6b
Jeff Lee <hellow.jeff@gmail.com>
2020-11-26 09:16:32 +0800

    fix print

--- TheGoStandardLibrary/02/showmestuff.go
+++ TheGoStandardLibrary/02/showmestuff.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"runtime"
-	"fmt"
 	"bufio"
+	"fmt"
 	"os"
+	"runtime"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("What is your name?")
 	text, _ := reader.ReadString('\n')
-	fmt.Println("Hello %v", text)
+	fmt.Printf("Hello %v", text)
 	fmt.Printf("We are using Go %v running in %s", runtime.Version(), runtime.GOOS)
 }
